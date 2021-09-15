a2a6dbd998e1a4bc91574ecfd49fad5f1f372c83
ostr00000 <ostr00000@gmail.com>
2021-09-15 03:39:22 +0200

    inc version, upload

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ packages = [
 
 setup(
     name='freecad-stubs',
-    version='1.0.2',
+    version='1.0.3',
     description="Python stubs for FreeCAD",
     long_description=readme,
     long_description_content_type='text/markdown',
