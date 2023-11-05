bb9006ef6441f7c0396da720b2364bbf43c50c2d
ostr00000 <ostr00000@gmail.com>
2023-11-05 01:40:47 +0100

    add debug function, use python3.12 to generate stubs

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-PYTHON=python3.11
+PYTHON=python3.12
 ENV_NAME=freecad_env
 PYTHON_ENV=./$(ENV_NAME)/bin/python
 
---
+++ lib/freecad_stub_gen/debug_functions.py
@@ -0,0 +1,18 @@
+import atexit
+from collections import defaultdict
+from typing import Hashable
+
+from freecad_stub_gen.util import OrderedSet
+
+printAllValues = defaultdict[str, OrderedSet](OrderedSet)
+
+
+def addPrintAllValue(topic: str, value: Hashable):
+    if topic not in printAllValues:
+        @atexit.register
+        def printValues():
+            print(f"Values for {topic}:")
+            for k in printAllValues[topic]:
+                print(f" - `{k}`")
+
+    printAllValues[topic].add(value)
