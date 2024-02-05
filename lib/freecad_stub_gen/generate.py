7ed580b3f67653a96bb6f5ff85e3ab9efa1958dd
ostr00000 <ostr00000@gmail.com>
2024-02-05 01:39:42 +0100

    fix issue #14: add missing module `FreeCADGui.PySideUic`

--- lib/freecad_stub_gen/generate.py
+++ lib/freecad_stub_gen/generate.py
@@ -112,6 +112,32 @@ Wrn = FreeCAD.Console.PrintWarning
         )
     )
 
+    pySideUic = sourcesRoot['FreeCADGui.PySideUic']
+    pySideUic += """
+_OptWid_t: typing.TypeAlias = qtpy.QtWidgets.QWidget | None
+
+@typing.overload
+def createCustomWidget(args: tuple[str]) -> _OptWid_t: ...
+@typing.overload
+def createCustomWidget(args: tuple[str, qtpy.QtWidgets.QWidget]) -> _OptWid_t: ...
+@typing.overload
+def createCustomWidget(args: tuple[str, qtpy.QtWidgets.QWidget, str]) -> _OptWid_t: ...
+
+@typing.overload
+def loadUi(args: tuple[str]) -> _OptWid_t: ...
+@typing.overload
+def loadUi(args: tuple[str, qtpy.QtCore.QObject | None]) -> _OptWid_t: ...
+
+class _UiGeneratedClass:
+    def setupUi(self, widget: qtpy.QtWidgets.QWidget) -> None: ...
+    def retranslateUi(self, widget: qtpy.QtWidgets.QWidget) -> None: ...
+
+_LoadRes_t: typing.TypeAlias =  tuple[_UiGeneratedClass, qtpy.QtWidgets.QWidget]
+
+def loadUiType(args: tuple[str]) -> _LoadRes_t | None: ...
+"""
+    pySideUic.imports.update(('typing', 'qtpy.QtCore', 'qtpy.QtWidgets'))
+
     for mod in (sourcePath / 'Mod').iterdir():
         moduleName = mod.name
         if moduleName in ('Test',):
